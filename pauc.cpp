0653a13be7a12f2b29a2c45272e88b0f40ca26b4
Xin Jin <xjin@rbox.me>
2016-06-04 23:51:38 -0700

    add noprint

--- pauc.cpp
+++ pauc.cpp
@@ -6,20 +6,25 @@ struct Options {
     bool print_only = false;
     std::string filename = "graph.in";
     size_t nthreads = 1;
-    bool gs = false;            // whether use Gauss-Seidel method
+    // whether use Gauss-Seidel method
+    bool gs = false;
+    // if true, then don't print the output; this is mainly for
+    // timing purposes
+    bool noprint = false;
 };
 
 void parseArgumentList(int argc, char *argv[], Options& options) {
     struct option ops[] = {
-        {"print", no_argument, NULL, 'p'},
+        {"print-matrix", no_argument, NULL, 'p'},
         {"file", required_argument, NULL, 'f'},
         {"threads", required_argument, NULL, 't'},
         {"gs", no_argument, NULL, 'g'},
+        {"no-print", no_argument, NULL, 'n'},
         {NULL, 0, NULL, 0},
     };
 
     while (true) {
-        int ch = getopt_long(argc, argv, "pf:t:g", ops, NULL);
+        int ch = getopt_long(argc, argv, "pf:t:gn", ops, NULL);
         if (ch == -1) break;
         switch (ch) {
         case 'p':
@@ -34,6 +39,9 @@ void parseArgumentList(int argc, char *argv[], Options& options) {
         case 'g':
             options.gs = true;
             break;
+		case 'n':
+			options.noprint = true;
+			break;
         default:
             cout << "Unrecognized or improperly supplied flag." << endl;
             exit(2);
@@ -59,7 +67,8 @@ int main(int argc, char *argv[]) {
     }
     Assignment assignment(options.filename, options.nthreads);
     assignment.auction();
-    assignment.printAssignment();
+	if (!options.noprint)
+		assignment.printAssignment();
 
     return 0;
 }
